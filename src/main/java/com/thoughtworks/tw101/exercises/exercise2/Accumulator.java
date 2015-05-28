a57d6a9506e18991eccd85e9c1efa3339d026558
Joye Nettles <nettlesjj@g.cofc.edu>
2015-05-28 16:54:21 -0400

    Completed exercise two: Counting times method is called

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,23 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
+/*
+Counts the number of times a method is called
+ */
 public class Accumulator {
+    private int total; // stores number of times method is called
+
+    /*
+    Prints the total number of times a method was called
+     */
+    public void total(){
+        System.out.println("Total: " + total);
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
@@ -4,10 +4,24 @@ package com.thoughtworks.tw101.exercises.exercise2;
 // when you call total(). Create a main method that calls increment() 5 times and then calls total().
 
 public class Main {
+
+    /*
+    Increases the total number of methods called by one
+    @param accumulator used to update the total of the accumulator instance
+    */
+    public static void increment(Accumulator accumulator){
+        accumulator.setTotal(accumulator.getTotal() +1);
+    }
+
     public static void main(String[] args) {
 
         Accumulator accumulator = new Accumulator();
 
+        // Calls increment five times
+        for(int i=0; i<5; i++){
+            increment(accumulator);
+        }
 
+        accumulator.total(); //Prints the total number of times increment is called
     }
 }
