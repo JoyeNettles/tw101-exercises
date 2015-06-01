d192ded6500ec7f43f84c634a60affba58611fa2
Joye Nettles <nettlesjj@g.cofc.edu>
2015-05-31 23:25:50 -0400

    Completed exercise three: printing sum of odd numbers in a range

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -2,7 +2,26 @@ package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
 
+    //  returns the sum of all odd integers between a starting and ending integer value.
     public int of(int start, int end) {
-        return -1;
+        int sum = 0;
+        while(start<end+1){
+            if (isOdd(start)) {
+                sum += start; // adds to sum
+            }
+            start++;
+        }
+        return sum;
+    }
+
+    /*
+ Checks to see if a number is odd
+  */
+    private static boolean isOdd(int number){
+        boolean isOdd = false;
+        if(number % 2 == 1 || number %2 == -1){
+            isOdd = true;
+        }
+        return isOdd;
     }
 }
